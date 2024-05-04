ace673000c89ab62aba9cfbc75c7934d8be9ca83
SandroBoka <sandro.boka1@gmail.com>
2024-05-04 19:58:36 +0200

    footer odvojen u svoj View, poboljsan StackView

---
+++ MovieApp/MovieDetailsFooterView.swift
@@ -0,0 +1,82 @@
+//
+//  MovieDetailsFooterView.swift
+//  MovieApp
+//
+//  Created by Sandro Boka on 04.05.2024..
+//
+
+import Foundation
+import UIKit
+
+class MovieDetailsFooterView: UIView {
+    
+    init(movieStruct: Movie.MovieStruct) {
+        super.init(frame: .zero)
+        setup(movieStruct: movieStruct)
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder){
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func setup(movieStruct: Movie.MovieStruct) {
+        self.backgroundColor = .white
+        
+        displayCrewInfo(movieStruct: movieStruct)
+    }
+    
+    private func displayCrewInfo(movieStruct: Movie.MovieStruct) {
+        let parentStack = UIStackView()
+        self.addSubview(parentStack)
+        parentStack.axis = .vertical
+        parentStack.alignment = .fill
+        parentStack.distribution = .fillEqually
+        parentStack.spacing = 30
+        parentStack.autoPinEdge(.leading, to: .leading, of: self, withOffset: 10)
+        parentStack.autoPinEdge(.top, to: .top, of: self, withOffset: 30)
+        parentStack.autoPinEdge(.trailing, to: .trailing, of: self)
+        
+        let numOfRows = (movieStruct.crewNames.count + 2) / 3
+        var index = 0
+        
+        for _ in 0...numOfRows - 1{
+            let innerStackView = UIStackView()
+            innerStackView.axis = .horizontal
+            innerStackView.alignment = .center
+            innerStackView.distribution = .fillEqually
+            parentStack.addArrangedSubview(innerStackView)
+            
+            let fields = [UIView(), UIView(), UIView()]
+            
+            for field in fields {
+                field.autoSetDimension(.height, toSize: 60)
+                innerStackView.addArrangedSubview(field)
+                
+                if index <= movieStruct.crewNames.count - 1 {
+                    let nameLabel = UILabel()
+                    nameLabel.text = movieStruct.crewNames[index]
+                    nameLabel.font = UIFont.systemFont(ofSize: 13.5, weight: .heavy)
+                    nameLabel.translatesAutoresizingMaskIntoConstraints = true
+                    nameLabel.numberOfLines = 0
+                    field.addSubview(nameLabel)
+                    nameLabel.autoPinEdge(.top, to: .top, of: field, withOffset: 5)
+                    nameLabel.autoPinEdge(.leading, to: .leading, of: field)
+                    nameLabel.autoPinEdge(.trailing, to: .trailing, of: field)
+                    
+                    let roleLabel = UILabel()
+                    roleLabel.text = movieStruct.crewRoles[index]
+                    roleLabel.font = UIFont.systemFont(ofSize: 15)
+                    field.addSubview(roleLabel)
+                    roleLabel.autoPinEdge(.top, to: .top, of: nameLabel, withOffset: 30)
+                    roleLabel.autoPinEdge(.left, to: .left, of: field)
+                    roleLabel.autoPinEdge(.trailing, to: .trailing, of: field)
+                    roleLabel.autoPinEdge(.bottom, to: .bottom, of: field)
+                    
+                    index += 1
+                }
+            }
+        }
+    }
+}
--- MovieApp/MovieDetailsViewController.swift
+++ MovieApp/MovieDetailsViewController.swift
@@ -14,12 +14,13 @@ class MovieDetailsViewController: UIViewController {
     
     var header: UIView!
     var content: UIView!
+    var footer: UIView!
     var whiteRectangle: UIView!
     var stackView: UIStackView!
     var movieCrewMembers: UILabel!
     var parentStack: UIStackView!
     
-    var movieId =  110357
+    var movieId =  111161
     var movie = Movie()
     
     
@@ -41,9 +42,7 @@ class MovieDetailsViewController: UIViewController {
         
         buildContent(movieStruct: movieStruct)
         
-        buildRectangle(movieStruct: movieStruct)
-        
-        buildRectangleLabels(movieStruct: movieStruct)
+        buildFooter(movieStruct: movieStruct)
     }
     
     private func buildHeader(movieStruct: Movie.MovieStruct) {
@@ -62,75 +61,15 @@ class MovieDetailsViewController: UIViewController {
         content.autoPinEdge(toSuperviewEdge: .leading)
         content.autoPinEdge(toSuperviewEdge: .trailing)
     }
-
-    
-    private func buildRectangle(movieStruct: Movie.MovieStruct) {
-        whiteRectangle = UIView()
-        whiteRectangle.backgroundColor = .white
-        view.addSubview(whiteRectangle)
-        whiteRectangle.autoPinEdge(.top, to: .bottom, of: content)
-        whiteRectangle.autoPinEdge(toSuperviewEdge: .leading)
-        whiteRectangle.autoPinEdge(toSuperviewEdge: .bottom)
-        whiteRectangle.autoPinEdge(toSuperviewEdge: .trailing)
-    }
     
     
-    private func displayCrewInfo() {
-        
-        var numOfRows = movie.crewNames.count / 3
-        if movie.crewNames.count % 3 != 0 {
-            numOfRows += 1
-        }
-        
-        for row in 0...numOfRows - 1 {
-            let infoStackView = UIStackView()
-            infoStackView.axis = .horizontal
-            infoStackView.alignment = .center
-            infoStackView.distribution = .fillEqually
-            parentStack.addArrangedSubview(infoStackView)
-            
-            let greenRectangle = UIView()
-            let pinkRectangle = UIView()
-            let yellowRectangle = UIView()
-            let rectangleList = [greenRectangle, pinkRectangle, yellowRectangle]
-            
-            for i in 0...2 {
-                rectangleList[i].autoSetDimension(.height, toSize: 60)
-                infoStackView.addArrangedSubview(rectangleList[i])
-                
-                for j in 0...movie.crewNames.count - 1 {
-                    if j == i + 3 * row {
-                        let nameLabel = UILabel()
-                        nameLabel.text = movie.crewNames[j]
-                        nameLabel.font = UIFont.systemFont(ofSize: 14, weight: .heavy)
-                        rectangleList[i].addSubview(nameLabel)
-                        nameLabel.autoPinEdge(.top, to: .top, of: rectangleList[i], withOffset: 5)
-                        nameLabel.autoPinEdge(.left, to: .left, of: rectangleList[i])
-                        let roleLabel = UILabel()
-                        roleLabel.text = movie.crewRoles[j]
-                        roleLabel.font = UIFont.systemFont(ofSize: 15)
-                        rectangleList[i].addSubview(roleLabel)
-                        roleLabel.autoPinEdge(.top, to: .top, of: rectangleList[i], withOffset: 30)
-                        roleLabel.autoPinEdge(.left, to: .left, of: rectangleList[i])
-                    }
-                }
-            }
-        }
-    }
-    
-    private func buildRectangleLabels(movieStruct: Movie.MovieStruct) {
-    
-        parentStack = UIStackView()
-        whiteRectangle.addSubview(parentStack)
-        parentStack.axis = .vertical
-        parentStack.alignment = .fill
-        parentStack.distribution = .fillEqually
-        parentStack.spacing = 30
-        parentStack.autoPinEdge(.leading, to: .leading, of: whiteRectangle, withOffset: 15)
-        parentStack.autoPinEdge(.top, to: .top, of: whiteRectangle, withOffset: 160)
-        parentStack.autoPinEdge(.trailing, to: .trailing, of: whiteRectangle, withOffset: -15)
-        
-        displayCrewInfo()
+    private func buildFooter(movieStruct: Movie.MovieStruct) {
+        footer = MovieDetailsFooterView(movieStruct: movieStruct)
+        view.addSubview(footer)
+        footer.autoPinEdge(toSuperviewEdge: .leading)
+        footer.autoPinEdge(toSuperviewEdge: .trailing)
+        footer.autoPinEdge(.top, to: .bottom, of: content)
+        footer.autoPinEdge(toSuperviewEdge: .bottom)
     }
 }
 
